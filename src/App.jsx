8fb348aef08ede533fbd817888c6bdc979040ebe
animes barman <animesbarman101@gmail.com>
2024-03-13 13:02:09 +0600

    add all link

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+import Blogs from './components/blogsData/Blogs'
+import BookMark from './components/bookMark/BookMark'
+import Header from './components/header/Header'
+
+function App() {
+
+
+  return (
+    <div className='w-[90%] mx-auto'>
+    <Header></Header>
+    <div className='md:flex md:gap-5 my-10'>
+    <Blogs></Blogs>
+    <BookMark></BookMark>
+    </div>
+    </div>
+  )
+}
+
+export default App;
